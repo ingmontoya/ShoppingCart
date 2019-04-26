8cd004ce2bd1c057bac7588b05c9271d72dc9675
Mauricio Montoya <mauricio.montoyav@gmail.com>
2019-04-25 21:18:23 -0500

    change to sendFile in server.js to deploy

--- server.js
+++ server.js
@@ -48,7 +48,8 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('public/build'));
 }
 app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 
